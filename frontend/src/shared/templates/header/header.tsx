bf289d08a4c588922ce15ae63535e90891ddd61b
Juan Camilo <grosso.juan@correounivalle.edu.co>
2025-04-10 09:44:13 -0500

    feat(shared-templates): agregar header y sidenav al layout principal

---
+++ frontend/src/shared/templates/header/header.tsx
@@ -0,0 +1,25 @@
+import './header.css';
+import { NavLink } from "react-router-dom";
+
+function Header() {
+    return (
+        <header className='header'>
+            <ul className='header-content'>
+                <li><NavLink to="" className='nav-item' data-bs-toggle="tooltip" data-bs-placement="bottom" data-bs-title="Home"><i className='bi bi-house-door-fill' /></NavLink></li>
+                <li className='dropdown'>
+                        <a className="nav-item "  role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <i className='bi bi-person-fill' ></i>
+                        </a>
+                        <ul className="dropdown-menu">
+                            <li><a className="dropdown-item text-center" href="#">Administrar datos</a></li>
+                            <li><a className="dropdown-item text-center" href="#">Cerrar sesión <i className="bi bi-box-arrow-in-right"></i></a></li>
+                        </ul>
+                    
+                </li>
+            </ul>
+        </header>
+
+    )
+}
+
+export default Header
