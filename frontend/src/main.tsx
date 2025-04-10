f0190ea2ced15e361d58937b0f44f7c1c22e6761
Juan Camilo <grosso.juan@correounivalle.edu.co>
2025-04-10 11:06:53 -0500

    feat(core): configurar punto de entrada con RouterProvider y StrictMode

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { createRoot } from 'react-dom/client'
+import { RouterProvider } from 'react-router';
 import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { route } from './core/config/route.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={route}/>
+  </StrictMode>
+
 )
