7d097d07946bdec738c28d12e34a2504992519bc
Juan Camilo <grosso.juan@correounivalle.edu.co>
2025-04-21 20:17:57 -0500

    feat(shared-components): agregar estructura definida del componente form-field

---
+++ frontend/src/shared/components/form-field/form-field.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+type FormFieldProps = {
+  label: string;
+  name: string;
+  type?: React.HTMLInputTypeAttribute;
+  value: string | number;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
+  disabled?: boolean;
+  className?: string;
+  placeholder?: string;
+  as?: "input" | "textarea";
+  readOnly?: boolean;
+};
+
+const FormField = ({
+  label,
+  name,
+  type = "text",
+  value,
+  onChange,
+  disabled = false,
+  className = "col-lg-6 col-12",
+  placeholder,
+  as = "input",
+  readOnly = false,
+}: FormFieldProps) => {
+  // Props comunes para ambos tipos de input
+  const commonProps = {
+    id: name,
+    name,
+    value,
+    disabled,
+    readOnly: readOnly || !onChange, 
+    className: "form-control",
+    placeholder: placeholder ? `Ingrese ${placeholder}` : undefined,
+    ...(onChange && { onChange }), 
+  };
+
+  return (
+    <div className={className}>
+      <label htmlFor={name} className="form-label">
+        {label}
+      </label>
+      
+      {as === "textarea" ? (
+        <textarea {...commonProps} />
+      ) : (
+        <input type={type} {...commonProps} />
+      )}
+    </div>
+  );
+};
+
+export default FormField;
