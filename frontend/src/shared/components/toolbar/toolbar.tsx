2f81b7d671e247471687ab72bf2465bc0f5d93e7
Juan Camilo <grosso.juan@correounivalle.edu.co>
2025-04-21 20:22:19 -0500

    feat(shared-components): agregar estilos de bootstrap a las etiquetas del componente toolbar

--- frontend/src/shared/components/toolbar/toolbar.tsx
+++ frontend/src/shared/components/toolbar/toolbar.tsx
@@ -1,21 +1,27 @@
 import "./toolbar.css"
 import { NavLink } from 'react-router-dom';
-function Toolbar() {
+
+export interface ToolBarProps {
+    children: React.ReactNode;
+}
+function Toolbar({ children }: ToolBarProps) {
 
     return (
-        <section className="d-flex justify-content-between">
-            <NavLink
-                to="add"
-                type="button"
-                className="btn btn-light fw-medium d-flex align-items-center gap-2"
-            >
-                <i className="bi bi-plus-circle fw-bold"></i>
-                <span>Agregar</span>
-            </NavLink>
-            <div className="position-relative col-md-3">
-                <input type="text" className="form-control border border-black border-opacity-50" placeholder="Buscar Cliente" />
-                <i className="btn position-absolute top-0 end-0 bi bi-search fw-bolder"></i>
-            </div>
+        <section className="d-flex flex-wrap gap-3 justify-content-between mb-3">
+           
+                <NavLink
+                    to="add"
+                    type="button"
+                    className="btn btn-shadow btn-light fw-medium d-flex align-items-center gap-2"
+                >
+                    <i className="bi bi-plus-circle fw-bold"></i>
+                    <span>Agregar</span>
+                </NavLink>
+            
+            <section className="col-lg-8 col-12 d-flex flex-wrap justify-content-end gap-3">
+                {children}
+            </section>
+
         </section>
     )
 }
