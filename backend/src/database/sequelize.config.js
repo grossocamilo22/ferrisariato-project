a1ec69e59537c5c91659734f2865ccef076f3d4a
Juan Camilo <grosso.juan@correounivalle.edu.co>
2025-04-26 23:30:48 -0500

    feature(sequelize) eliminar archvios de configuración de sequelize

--- backend/src/database/sequelize.config.js
+++
@@ -1,9 +0,0 @@
-require('ts-node/register'); // Permite cargar archivos TS en tiempo real
-const configs = require('../configs.ts');
-
-export const username = configs.DB_USERNAME;
-export const password = configs.DB_PASSWORD;
-export const database = configs.DB_DATABASE;
-export const host = configs.DB_HOST;
-export const dialect = 'postgres';
-export const port = 5432;
